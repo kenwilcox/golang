b079df985c9b6553acfb81e1f647888f0a820f1c
Ken Wilcox <wilcoxk@medamerica.com>
2017-05-08 21:37:42 -0600

    updated hello

--- hello/hello.go
+++ hello/hello.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-  "fmt" 
-  "github.com/kenwilcox/stringutil"
+	"fmt"
+	"github.com/kenwilcox/stringutil"
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Printf(stringutil.Reverse("!oG ,olleH") + "\n")
+	fmt.Printf(stringutil.Reverse("!nuf si sihT"))
 }
