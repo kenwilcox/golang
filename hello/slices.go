c80e713051a1fd0d17f2aed55707aaf8440e2aa6
Ken Wilcox <wilcoxk@medamerica.com>
2018-02-26 15:34:13 -0700

    slice literals

--- hello/slices.go
+++ hello/slices.go
@@ -25,4 +25,23 @@ func main() {
 	b[0] = "XXX"
 	fmt.Println(a, b)
 	fmt.Println(names)
+
+	q := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println(q)
+
+	r := []bool{true, false, true, true, false, true}
+	fmt.Println(r)
+
+	st := []struct {
+		i int
+		b bool
+	}{
+		{2, true},
+		{3, false},
+		{5, true},
+		{7, true},
+		{11, false},
+		{13, true},
+	}
+	fmt.Println(st)
 }
