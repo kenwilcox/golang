27c3652c71a3e04dfd572f6add77ee8ed83d44a7
Ken Wilcox <wilcoxk@medamerica.com>
2018-02-26 15:17:33 -0700

    literals

--- hello/structs.go
+++ hello/structs.go
@@ -1,15 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 type Vertex struct {
 	X int
 	Y int
 }
 
+var (
+	v1 = Vertex{1, 2}
+	v2 = Vertex{X: 1}
+	v3 = Vertex{}
+	p  = &Vertex{1, 2}
+)
+
 func main() {
-	v := Vertex{1, 2}
-	p := &v
-	p.X = 1e9
-	fmt.Println(v)
+	fmt.Println(v1, p, v2, v3)
+	fmt.Println("v1 = ", reflect.TypeOf(v1))
+	fmt.Println("p = ", reflect.TypeOf(p))
 }
