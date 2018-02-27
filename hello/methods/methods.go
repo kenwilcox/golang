4e5c25dd0b849edab7450c9626a496bf25e1fe8c
Ken Wilcox <wilcoxk@medamerica.com>
2018-02-26 20:25:17 -0700

    methods

---
+++ hello/methods/methods.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+type MyFloat float64
+
+func (v Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func Abs(v Vertex) float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	v := Vertex{3, 4}
+	fmt.Println(v.Abs())
+
+	fmt.Println(Abs(v))
+
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
