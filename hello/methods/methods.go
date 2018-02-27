ee6e7c987db1f2216a680a20578cd2feb2586e73
Ken Wilcox <wilcoxk@medamerica.com>
2018-02-26 20:47:25 -0700

    methods

--- hello/methods/methods.go
+++ hello/methods/methods.go
@@ -19,6 +19,16 @@ func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func Scale(v *Vertex, f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 func (f MyFloat) Abs() float64 {
 	if f < 0 {
 		return float64(-f)
@@ -34,4 +44,10 @@ func main() {
 
 	f := MyFloat(-math.Sqrt2)
 	fmt.Println(f.Abs())
+
+	v.Scale(10)
+	fmt.Println(v.Abs())
+
+	Scale(&v, 10)
+	fmt.Println(Abs(v))
 }
